5e838cc1f0b4b2d83a78f1b7b3444775d255b80c
Gabriel Borges <gabriel.bgs00@gmail.com>
2020-10-18 18:07:51 -0300

    env variable done

---
+++ frontend/src/Keys.js
@@ -0,0 +1,5 @@
+const clientId = process.env.REACT_APP_CLIENT_ID
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET
+const redirectUri = process.env.REACT_APP_REDIRECT_URI
+
+export { clientId, clientSecret, redirectUri }
