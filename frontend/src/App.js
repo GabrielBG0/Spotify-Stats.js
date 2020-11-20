3a35ea16a32b3651632d9d96e39f0f4f1c8f92cb
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2020-11-20 13:25:43 -0300

    será??

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes'
-import Header from '../Header'
-import Footer from '../Footer'
+import Header from './pages/Header'
+import Footer from './pages/Footer'
 import './global.css'
 
 function App() {
